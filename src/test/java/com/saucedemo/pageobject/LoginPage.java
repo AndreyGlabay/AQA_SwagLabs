28e8ced0919e5e5813124222685a03ce2b5de57f
Andriy Glabay <rishelevsky@gmail.com>
2024-03-07 19:34:46 +0200

    Create test class /src/java/com.saucedemo/test/LoginPageElementsTest.java and implements tests for (1) Check Elemenyts on the Login page on Chrome and FireFox browsers; (2) Check Login Page's title on Chrome and FireFox browsers

--- src/test/java/com/saucedemo/pageobject/LoginPage.java
+++ src/test/java/com/saucedemo/pageobject/LoginPage.java
@@ -6,31 +6,68 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage { // (2.1) Create the test class and implement Page Object for the Login flow.
     private WebDriver driver;
-    private static final String USERNAME_LOCATOR = "//*[@id=\"user-name\"]";
-    private static final String PASSWORD_LOCATOR = "//*[@id=\"password\"]";
-    private static final String BUTTON_LOCATOR = "//*[@id=\"login-button\"]";
-    private static final String MESSAGE_LOCATOR = "//h3[@data-test=\"error\"]";
+    public static final String USERNAME_LOCATOR = "//*[@id=\"user-name\"]"; // XPath
+    private static final String PASSWORD_LOCATOR = "//*[@id=\"password\"]"; // XPath
+    private static final String BUTTON_LOCATOR = "//*[@id=\"login-button\"]"; // XPath
+    private static final String MESSAGE_LOCATOR = "//h3[@data-test=\"error\"]"; // XPath
+    private static final String LOGO_LOCATOR = ".login_logo"; // CSS Selector
+    private static final String BOT_LOCATOR = ".bot_column"; // CSS Selector
+
+    private WebElement inputUsername;
+    private WebElement inputPassword;
+    private WebElement buttonLogin;
+    private WebElement messageError;
+    private WebElement imageLogo;
+    private WebElement imageBot;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        userNameInput = driver.findElement(By.xpath(USERNAME_LOCATOR));
-        passwordInput = driver.findElement(By.xpath(PASSWORD_LOCATOR));
-        button = driver.findElement(By.xpath(BUTTON_LOCATOR));
+        inputUsername = driver.findElement(By.xpath(USERNAME_LOCATOR));
+        inputPassword = driver.findElement(By.xpath(PASSWORD_LOCATOR));
+        buttonLogin = driver.findElement(By.xpath(BUTTON_LOCATOR));
+        imageLogo = driver.findElement(By.cssSelector(LOGO_LOCATOR));
+        imageBot = driver.findElement(By.cssSelector(BOT_LOCATOR));
     }
 
-    private WebElement userNameInput;
-    private WebElement passwordInput;
-    private WebElement button;
-    private WebElement message;
+    // Check if the input Username is on the page
+    public boolean checkIsInputUsernamePresent() {
+        return inputUsername.isDisplayed();
+    }
 
+    // Check if the input Password is on the page
+    public boolean checkIsInputPasswordPresent() {
+        return inputPassword.isDisplayed();
+    }
+
+    // Check if the button Login is on the page
+    public boolean checkIsButtonLoginPresent() {
+        return buttonLogin.isDisplayed();
+    }
+
+    // Check if Logo image is on the page
+    public boolean checkIsImageLogoPresent() {
+        return imageLogo.isDisplayed();
+    }
+
+    // Check if Bot image is on the page
+    public boolean checkIsImageBotPresent() {
+        return imageBot.isDisplayed();
+    }
+
+
+
+
+
+    // Login flow.
     public void login(String username, String password) {
-        userNameInput.sendKeys(username);
-        passwordInput.sendKeys(password);
-        button.click();
+        inputUsername.sendKeys(username);
+        inputPassword.sendKeys(password);
+        buttonLogin.click();
     }
 
+    // Get and check Error Message.
     public boolean checkMessage(String str) {
-        message = driver.findElement(By.xpath(MESSAGE_LOCATOR));
-        return (message.getText().contains(str));
+        messageError = driver.findElement(By.xpath(MESSAGE_LOCATOR));
+        return (messageError.getText().contains(str));
     }
 }
--- src/test/java/com/saucedemo/test/LoginPageElementsTests.java
+++ src/test/java/com/saucedemo/test/LoginPageElementsTests.java
@@ -7,23 +7,15 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class LoginPageTests {
+public class LoginPageElementsTests {
     WebDriver driver;
     public String gridUrl = "http://192.168.0.102:4444";
     public String baseUrl = "https://www.saucedemo.com/v1/";
-
-
-
-//    @BeforeTest
-//    public void startChromeInstance() throws MalformedURLException {
-//
-//    }
 
     @AfterTest
     public void stopChromeInstance() throws InterruptedException {
@@ -46,8 +38,8 @@ public class LoginPageTests {
 
     @Test (testName = "FF_CheckElements")
     public void findElementsFirefox() throws InterruptedException, MalformedURLException {
-        FirefoxOptions options = new FirefoxOptions();                // New instance for GC browser;
-        driver = new RemoteWebDriver(new URL(gridUrl), options);    // Driver initialization on SeleniumGrid using GC;
+        FirefoxOptions options = new FirefoxOptions();              // New instance for FF browser;
+        driver = new RemoteWebDriver(new URL(gridUrl), options);    // Driver initialization on SeleniumGrid using FF;
         driver.get(baseUrl);                                        // Open web page by URL;
         var loginPage = new LoginPage(driver);                      // Make an instance of web page;
         Assert.assertTrue(loginPage.checkIsInputUsernamePresent(), "Username input is missing on the page");
@@ -57,14 +49,21 @@ public class LoginPageTests {
         Assert.assertTrue(loginPage.checkIsImageBotPresent(), "Bot image is missing on the page");
     }
 
-//    @Test (testName = "Check Title")
-//    public void checkTitle() {
-//        driver.get(pageLogin);
-//    }
-//
-//    @Test (testName = "Check Login")
-//    public void checkLogin() {
-//        driver.get(pageLogin);
-//    }
+    @Test (testName = "GC_CheckTitle")
+    public void checkTitleChrome() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();                // New instance for GC browser;
+        driver = new RemoteWebDriver(new URL(gridUrl), options);    // Driver initialization on SeleniumGrid using GC;
+        driver.get(baseUrl);                                        // Open web page by URL;
+        var title = driver.getTitle();                              // Represent the title of web page;
+        Assert.assertEquals(title, "Swag Labs");
+    }
 
+    @Test (testName = "FF_CheckTitle")
+    public void checkTitleFirefox() throws MalformedURLException {
+        FirefoxOptions options = new FirefoxOptions();              // New instance for GC browser;
+        driver = new RemoteWebDriver(new URL(gridUrl), options);    // Driver initialization on SeleniumGrid using GC;
+        driver.get(baseUrl);                                        // Open web page by URL;
+        var title = driver.getTitle();                              // Represent the title of web page;
+        Assert.assertEquals(title, "Swag Labs");
+    }
 }
